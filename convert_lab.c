a9c9da9497b1f1b69c2d211ee0671387b991ba55
tsunekoromochi <happymusic.5.10@gmail.com>
2019-05-07 21:31:51 +0900

    Please enter the commit message for your changes. Lines starting

--- convert_lab.c
+++ convert_lab.c
@@ -13,6 +13,7 @@
 #define HEIGHT_END2   90
 #define WIDTH_END2    190
 
+a
 
 void ipr_load_ppm(unsigned char image[][WIDTH][3], const char path[]);
 void ipr_save_ppm(unsigned char image[][WIDTH][3], const char path[]);
